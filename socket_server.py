5751c5f9b46c6459371038a150ddc0733233cea9
amin tahmasebi <amintahmasebi479@gmail.com>
2023-10-11 22:56:46 +0330

    add socket server source code

---
+++ socket_server.py
@@ -0,0 +1,34 @@
+import socket
+from typing import Final
+
+
+def start_server() -> None:
+    global HOST, PORT
+    server_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    print(f"Server listening on {HOST}:{PORT}")
+    connection, addr = server_socket.accept()
+
+    with connection:
+        print(f"{addr} connect to server")
+
+        while True:
+            data_recv: bytes = connection.recv(1024)
+            print(f"Receive data -> {data_recv}")
+
+            if not data_recv:
+                break
+
+            connection.sendall(data_recv)
+
+        server_socket.close()
+
+
+if __name__ == "__main__":
+    HOST: Final[str] = "127.0.0.1"
+    PORT: Final[int] = 64321
+
+    start_server()
